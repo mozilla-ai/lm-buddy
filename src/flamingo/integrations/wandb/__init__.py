a72fd59ae272389b44f784c4ad32811c20ce08c0
Sean Friedowitz <sean@mozilla.ai>
2024-01-22 13:34:08 -0800

    put cofnigs in own file

--- src/flamingo/integrations/wandb/__init__.py
+++ src/flamingo/integrations/wandb/__init__.py
@@ -1,4 +1,5 @@
 # ruff: noqa: I001
-from flamingo.integrations.wandb.configs import *
+from flamingo.integrations.wandb.run_config import WandbRunConfig
+from flamingo.integrations.wandb.artifact_config import WandbArtifactConfig
 from flamingo.integrations.wandb.artifact_utils import *
 from flamingo.integrations.wandb.run_utils import *
---
+++ src/flamingo/integrations/wandb/artifact_config.py
@@ -0,0 +1,16 @@
+from flamingo.types import BaseFlamingoConfig
+
+
+class WandbArtifactConfig(BaseFlamingoConfig):
+    """Configuration required to retrieve an artifact from W&B."""
+
+    name: str
+    version: str = "latest"
+    project: str | None = None
+    entity: str | None = None
+
+    def wandb_path(self) -> str:
+        """String identifier for the asset on the W&B platform."""
+        path = "/".join(x for x in [self.entity, self.project, self.name] if x is not None)
+        path = f"{path}:{self.version}"
+        return path
