e14bc1839ecadc9fa5d6170f1e8cff98be6a3128
Vicki Boykis <vicki@mozilla.ai>
2024-02-08 11:03:03 -0500

    clean up conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,21 +7,14 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = "flamingo"
-copyright = "2024, Sean Friedowitz, Vicki Boykis, Aaron Gonzales"
 author = "Sean Friedowitz, Vicki Boykis, Aaron Gonzales"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    "sphinx.ext.mathjax",
-    "sphinx.ext.autodoc",
-    "sphinx.ext.coverage",
-    "sphinx.ext.napoleon",
-    "sphinx.ext.autosectionlabel",
     "nbsphinx",
     "myst_parser",
-    "sphinx_copybutton",
 ]
 
 # use language set by highlight directive if no language is set by role
