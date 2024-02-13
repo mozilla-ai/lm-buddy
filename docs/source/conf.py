131a0ea738f81731dda8521bdbe5ccdbf40ccf4d
Vicki Boykis <vicki@mozilla.ai>
2024-02-12 20:25:50 -0500

    GH actions for docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,6 +15,7 @@ author = "Sean Friedowitz, Vicki Boykis, Aaron Gonzales"
 extensions = [
     "nbsphinx",
     "myst_parser",
+    "sphinx_codeautolink",
 ]
 
 # use language set by highlight directive if no language is set by role
@@ -22,6 +23,9 @@ inline_highlight_respect_highlight = False
 
 # use language set by highlight directive if no role is set
 inline_highlight_literals = False
+
+# Set deeplinks
+myst_heading_anchors = 3
 
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
@@ -32,4 +36,4 @@ source_suffix = [".rst", ".md"]
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "furo"
-html_static_path = ["_static"]
+html_static_path = []
