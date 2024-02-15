9feab7a0a6a0e83116e34bee8da340f1c209bfd0
Sean Friedowitz <sean@mozilla.ai>
2024-02-14 18:39:34 -0700

    move run method

--- src/lm_buddy/__init__.py
+++ src/lm_buddy/__init__.py
@@ -1,21 +1,3 @@
-from lm_buddy.integrations.wandb import ArtifactLoader, WandbArtifactLoader
-from lm_buddy.jobs.common import LMBuddyJobConfig
-from lm_buddy.jobs.finetuning import FinetuningJobConfig, run_finetuning
-from lm_buddy.jobs.lm_harness import LMHarnessJobConfig, run_lm_harness
-from lm_buddy.jobs.simple import SimpleJobConfig, run_simple
+from lm_buddy.jobs.run import run
 
-
-def run(
-    config: LMBuddyJobConfig,
-    *,
-    artifact_loader: ArtifactLoader = WandbArtifactLoader(),
-) -> None:
-    match config:
-        case SimpleJobConfig() as simple_config:
-            run_simple(simple_config, artifact_loader)
-        case FinetuningJobConfig() as finetuning_config:
-            run_finetuning(finetuning_config, artifact_loader)
-        case LMHarnessJobConfig() as lm_harness_config:
-            run_lm_harness(lm_harness_config, artifact_loader)
-        case _:
-            raise ValueError(f"Received invalid job configuration: {config}")
+__all__ = ["run"]
--- src/lm_buddy/cli/run.py
+++ src/lm_buddy/cli/run.py
@@ -5,8 +5,9 @@ from lm_buddy.jobs.finetuning import FinetuningJobConfig
 from lm_buddy.jobs.lm_harness import LMHarnessJobConfig
 from lm_buddy.jobs.simple import SimpleJobConfig
 
-# TODO: Do we collapse all these commands into a single CLI run command?
-# Need to figure out how to polymorphically deserialize the config classes?
+# TODO: We should probably collapse all these commands into a single CLI "lm_buddy run" command
+# - Need to figure out best way to polymorphically deserialize the job config classes
+# - Do we just add type discriminators at the job config level?
 
 
 @click.group(name="run", help="Run an LM Buddy job.")
@@ -16,20 +17,20 @@ def group():
 
 @group.command("simple", help="Run the simple test job.")
 @click.option("--config", type=str)
-def simple_command(config: str) -> None:
+def run_simple(config: str) -> None:
     config = SimpleJobConfig.from_yaml_file(config)
     lm_buddy.run(config)
 
 
 @group.command("finetuning", help="Run the HuggingFace LLM finetuning job.")
 @click.option("--config", type=str)
-def finetuning_command(config: str) -> None:
+def run_finetuning(config: str) -> None:
     config = FinetuningJobConfig.from_yaml_file(config)
     lm_buddy.run(config)
 
 
 @group.command("lm-harness", help="Run the lm-harness evaluation job.")
 @click.option("--config", type=str)
-def lm_harness_command(config: str) -> None:
+def run_lm_harness(config: str) -> None:
     config = LMHarnessJobConfig.from_yaml_file(config)
     lm_buddy.run(config)
