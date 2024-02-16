06924cce9c0ba87064e805c36a9b8dfff4a61741
Sean Friedowitz <sean@mozilla.ai>
2024-02-16 13:18:14 -0700

    remove return

--- src/lm_buddy/jobs/entrypoints/simple.py
+++ src/lm_buddy/jobs/entrypoints/simple.py
@@ -8,11 +8,10 @@ def get_magic_number(config: SimpleJobConfig) -> int:
     return config.magic_number
 
 
-def run_simple(config: SimpleJobConfig) -> int:
+def run_simple(config: SimpleJobConfig):
     """A simple entrypoint to demonstrate the Ray interface."""
     # Connect to the Ray cluster (if not already running)
     ray.init(ignore_reinit_error=True)
     # Run dummy remote task
     magic_number = ray.get(get_magic_number.remote(config))
     print(f"The magic number is {magic_number}")
-    return magic_number
