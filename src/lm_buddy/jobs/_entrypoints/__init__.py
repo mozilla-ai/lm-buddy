c99c56ff0b81e0c578fde48843a6e4983d8ab274
Imtihan Ahmed <imtihan@mozilla.ai>
2024-03-12 15:00:34 -0400

    moved files fixed conflicts in __init__ files for config and entrypoints

--- src/lm_buddy/jobs/_entrypoints/__init__.py
+++ src/lm_buddy/jobs/_entrypoints/__init__.py
@@ -1,5 +1,12 @@
 from lm_buddy.jobs._entrypoints.finetuning import run_finetuning
 from lm_buddy.jobs._entrypoints.lm_harness import run_lm_harness
+<<<<<<< HEAD
 from lm_buddy.jobs._entrypoints.prometheus import run_prometheus
 
 __all__ = ["run_finetuning", "run_lm_harness", "run_prometheus"]
+=======
+from lm_buddy.jobs._entrypoints.ragas import run_ragas_evaluation
+from lm_buddy.jobs._entrypoints.simple import run_simple
+
+__all__ = ["run_finetuning", "run_lm_harness", "run_simple", "run_ragas_evaluation"]
+>>>>>>> ca0e1d6 (moved files)
--- src/lm_buddy/jobs/configs/__init__.py
+++ src/lm_buddy/jobs/configs/__init__.py
@@ -5,9 +5,20 @@ from lm_buddy.jobs.configs.lm_harness import (
     LMHarnessJobConfig,
     LocalChatCompletionsConfig,
 )
+<<<<<<< HEAD
 from lm_buddy.jobs.configs.prometheus import PrometheusEvaluationConfig, PrometheusJobConfig
 
 EvaluationJobConfig = LMHarnessJobConfig | PrometheusJobConfig
+=======
+from lm_buddy.jobs.configs.ragas import (
+    RagasConfig,
+    RagasEvaluationDatasetConfig,
+    RagasEvaluationJobConfig,
+    RagasRayConfig,
+    RagasvLLMJudgeConfig,
+)
+from lm_buddy.jobs.configs.simple import SimpleJobConfig
+>>>>>>> ca0e1d6 (moved files)
 
 __all__ = [
     "LMBuddyJobConfig",
@@ -16,7 +27,17 @@ __all__ = [
     "LMHarnessEvaluationConfig",
     "LMHarnessJobConfig",
     "LocalChatCompletionsConfig",
+<<<<<<< HEAD
     "PrometheusEvaluationConfig",
     "PrometheusJobConfig",
     "EvaluationJobConfig",
+=======
+    "SimpleJobConfig",
+    "RagasEvaluationJobConfig",
+    "RagasConfig",
+    "RagasEvaluationDatasetConfig",
+    "RagasRayConfig",
+    "run_ragas_evaluation",
+    "RagasvLLMJudgeConfig",
+>>>>>>> ca0e1d6 (moved files)
 ]
