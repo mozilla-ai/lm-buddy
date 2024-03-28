0c21e33a9bfa0170f40973e62574a9d9e7928be3
Vicki Boykis <vicki@mozilla.ai>
2024-03-28 14:29:53 -0400

    fix yaml

--- src/lm_buddy/jobs/_entrypoints/__init__.py
+++ src/lm_buddy/jobs/_entrypoints/__init__.py
@@ -2,4 +2,4 @@ from lm_buddy.jobs._entrypoints.finetuning import run_finetuning
 from lm_buddy.jobs._entrypoints.lm_harness import run_lm_harness
 from lm_buddy.jobs._entrypoints.prometheus import run_prometheus
 
-__all__ = ["run_finetuning", "run_lm_harness", "run_prometheus"]
+__all__ = ["run_finetuning", "run_lm_harness", "run_prometheus", "run_ray_serve"]
