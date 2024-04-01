1390e317d22bdde0480fc275d2f7950aa4fb9b95
Sean Friedowitz <sean@mozilla.ai>
2024-04-01 13:02:20 -0700

    add docstring

--- src/lm_buddy/cli/utils.py
+++ src/lm_buddy/cli/utils.py
@@ -7,6 +7,11 @@ ConfigType = TypeVar("ConfigType", bound=LMBuddyJobConfig)
 
 
 def parse_config_option(config_cls: type[ConfigType], config: str) -> ConfigType:
+    """Parse the config option string from the CLI.
+
+    If it corresponds to a path that exists, attempt to load the config from YAML file.
+    If not, attempt to parse it as a JSON string.
+    """
     if Path(config).exists():
         return config_cls.from_yaml_file(config)
     else:
