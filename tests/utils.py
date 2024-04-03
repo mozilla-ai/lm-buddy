c33afd9baedc2b4dc5b4e95162276388903fdaf2
Sean Friedowitz <sean@mozilla.ai>
2024-04-03 10:36:47 -0700

    update logger class

--- tests/utils.py
+++ tests/utils.py
@@ -23,21 +23,15 @@ class FakeArtifactLoader:
         self.entity = entity
         self._storage: dict[str, wandb.Artifact] = dict()
 
-    @staticmethod
-    def set_artifact_attributes(
-        artifact: wandb.Artifact,
-        project: str = "test-project",
-        entity: str = "test-entity",
-        version: str = "latest",
-    ) -> wandb.Artifact:
-        artifact._project = project
-        artifact._entity = entity
-        artifact._version = version
+    def _set_commit_attributes(self, artifact: wandb.Artifact) -> wandb.Artifact:
+        artifact._project = self.project
+        artifact._entity = self.entity
+        artifact._version = "latest"
         artifact._state = ArtifactState.COMMITTED
 
         # W&B does this after logging an artifact
-        name_contains_version = len(artifact.name.split(":")) > 1
-        if not name_contains_version:
+        name_has_version = len(artifact.name.split(":")) > 1
+        if not name_has_version:
             artifact._name = f"{artifact._name}:{artifact._version}"
 
         return artifact
@@ -53,6 +47,6 @@ class FakeArtifactLoader:
 
     def log_artifact(self, artifact: wandb.Artifact) -> wandb.Artifact:
         """Store the artifact in-memory and update its attributes to mimic the W&B platform."""
-        artifact = self.set_artifact_attributes(artifact)
+        artifact = self._set_commit_attributes(artifact)
         self._storage[artifact.qualified_name] = artifact
         return artifact
