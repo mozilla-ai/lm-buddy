76097547df859d4a6da80da01a6d5abef9b636e4
Sean Friedowitz <sean@mozilla.ai>
2024-04-04 15:00:11 -0700

    remove constant

--- src/lm_buddy/constants.py
+++ src/lm_buddy/constants.py
@@ -1,9 +1,7 @@
 import os
 from pathlib import Path
 
-STORAGE_PATH_ENVIRONMENT_VARIABLE: str = "LM_BUDDY_STORAGE"
-
 DEFAULT_STORAGE_PATH: str = os.getenv(
-    STORAGE_PATH_ENVIRONMENT_VARIABLE,
+    "LM_BUDDY_STORAGE",
     str(Path.home() / "lm_buddy_results"),
 )
